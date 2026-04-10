fd4152b3c450a938647ddf95ec006db1a3483836
David <da3909622@gmail.com>
2026-04-10 14:36:07 -0600

    add change

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -24,7 +24,7 @@ Route::post('/seed', function () {
         ], 500);
     }
 });
-
+ Route::get('users', [App\Http\Controllers\UserController::class, 'index']);
 // Rutas Protegidas (Requieren Token de Sanctum)
 Route::middleware('auth:sanctum')->group(function () {
     
